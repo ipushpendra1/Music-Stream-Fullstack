993d7ec3ad4a49d3ce9d692ca5e6f1849b7fd51e
pushpendra <pushpendra090804@gmail.com>
2025-06-26 16:56:43 +0530

    updated- services and .env

--- backend/src/services/storage.service.js
+++ backend/src/services/storage.service.js
@@ -1,9 +1,28 @@
 import ImageKit from 'imagekit';
 import dotenv from 'dotenv';
 
+
+dotenv.config();
 var imagekit = new ImageKit({
-    publicKey: "your_public_api_key",
-    privateKey: "your_private_api_key",
-    urlEndpoint: "https://ik.imagekit.io/your_imagekit_id"
+    publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+    privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+    urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT
 
 })
+
+function uploadAudioFileFromStream(fileStream) {
+    return new Promise((resolve, reject) => {
+        imagekit.upload({
+            file: fileStream,
+            fileName: "audio-file-" + Date.now() + ".mp3",
+            folder: "/audio-files/"
+        }, function (error, result) {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(result);
+            }
+        })
+
+    })
+}

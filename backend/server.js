5335890e121790ef9809683fb3e457e3a7ed37b4
pushpendra <pushpendra090804@gmail.com>
2025-07-20 01:27:28 +0530

    edit server

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,14 @@ import config from "./src/config/config.js";
 
 connectDb();
 const PORT = config.PORT || 3000;
+
+
+app.get('/', (req, res)=>{
+          res.send({
+            activeStatus:true,
+            error:false,
+          })
+})
 app.listen(PORT, () => {
     console.log("Server is running on port 3000");
 

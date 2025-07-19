8ec5e734939183969dc70b0dbeb9b02b66bfddce
pushpendra <pushpendra090804@gmail.com>
2025-07-20 01:56:31 +0530

    add config file

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,10 @@
 
 import connectDb from "./src/db/db.js";
 import app from "./src/app.js";
-import config from "./src/config/config.js";
 
 connectDb();
-const PORT = config.PORT || 3000;
 
-
-app.get('/', (req, res)=>{
-          res.send({
-            activeStatus:true,
-            error:false,
-          })
-})
-app.listen(PORT, () => {
+app.listen(3000, () => {
     console.log("Server is running on port 3000");
 
 })
--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -7,7 +7,7 @@ const config = {
       IMAGEKIT_PUBLIC_KEY: process.env.IMAGEKIT_PUBLIC_KEY,
       IMAGEKIT_PRIVATE_KEY: process.env.IMAGEKIT_PRIVATE_KEY,
       IMAGEKIT_URL_ENDPOINT: process.env.IMAGEKIT_URL_ENDPOINT,
-      PORT: process.env.PORT,
+
 }
 
 export default config
